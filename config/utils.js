9a5877e7d1a0e07ff3acba4130704c3dbf523254
liangjinxing <jinxing.liang@happy-seed.com>
2021-12-14 12:29:18 +0800

    debug

--- config/utils.js
+++ config/utils.js
@@ -10,7 +10,7 @@ exports.createNotifierCallback = () => {
 
     const error = errors[0]
     const filename = error.file && error.file.split('!').pop()
-
+    console.error(error.message)
     notifier.notify({
       title: packageConfig.name,
       message: severity + ': ' + error.name,
